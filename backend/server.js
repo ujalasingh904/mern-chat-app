4a90a300f4c8171caac0254b31cf19f6e2e76895
ujala <singhujala2004@gmail.com>
2024-06-27 15:27:21 +0530

    "Updated backend server.js to serve frontend dist files, added dependencies to frontend package.json, and modified scripts in package.json"

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import messageRoutes from "./routes/message.route.js"
 import userRoutes from "./routes/userRoutes.route.js"
 import cors from "cors"
 import { app, server } from "./socket/socket.js"
+import path from "path"
 dotenv.config()
 
 mongoose
@@ -24,8 +25,16 @@ app.use(cors(
 ))
 const port = process.env.PORT || 5000
 
+const __dirname = path.resolve();
+
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist" ,"index.html"));
+})
 
 server.listen(port, () => console.log(`listening on port:`, port))
